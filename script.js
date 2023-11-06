8d077d993c757a2d0078320f2a1280b8210c98c2
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-06 11:29:46 +0200

    adding delete and done button, fixing small things, adding styles to new buttons

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ let input = document.querySelector('#input');
 let list = document.querySelector('#list');
 
 input.addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
-    let li = document.createElement('li');
-    li.textContent = input.value;
-
-    // Вставляем новый элемент после списка
-    list.appendChild(li);
-
-    input.value = '';
-  }
+	if (event.key == 'Enter') {
+		let li = document.createElement('li');
+		
+		let task = document.createElement('span');
+    task.classList.add('task');
+		task.textContent = this.value;
+		li.appendChild(task);
+		
+		let remove = document.createElement('span');
+		remove.classList.add('remove');
+    remove.textContent = 'удалить';
+		li.appendChild(remove);
+		
+		let mark = document.createElement('span');
+		mark.classList.add('mark');
+    mark.textContent = 'сделано';
+		li.appendChild(mark);
+		
+		list.appendChild(li);
+		
+		this.value = '';
+	}
 });
